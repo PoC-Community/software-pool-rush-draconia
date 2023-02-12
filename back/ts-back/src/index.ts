ddc6c66dd9367c3fa52d7db734dbd2ff8eaf929a
Auguste Frater <augustefrater@gmail.com>
2023-02-12 11:52:34 +0100

    [NEWS] worst merge ever done

---
+++ back/ts-back/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { Application, Request, Response } from "express";
+import cors from "cors";
+import { exit } from "process";
+require('dotenv').config();
+import { authenticateToken } from "./middlewares/authenticateToken";
+import { login } from "./services/login";
+import { userInfo } from "./services/userInfo";
+import { register } from "./services/register";
+
+// ERROR HANDLING
+if (process.env.IS_ENV_SETUP === undefined) {
+  console.log(".env not setup. Create your env as asked in the documentation and add the IS_ENV_SETUP variable in it.");
+  exit();
+}
+
+//Express app
+const app: Application = express();
+
+//convert incoming data into json
+app.use(express.json());
+app.use(cors()); // Semblerait que ça serve à accepter les co de tous les domaines, ou que de quelques uns
+app.use(express.urlencoded({ extended: true })); // sert à lire le utf-8 je crois
+
+//login route
+app.post("/login", login);
+app.post("/register", register);
+
+app.get("/userInfo", authenticateToken, userInfo);
+
+app.listen(8080, () => console.log("running on 8080"));
